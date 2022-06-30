695a8fee97c614b36048e1d81802fb024dbbb8f5
Bobby Dhillon <bdhill@arista.com>
2022-06-29 21:11:07 -0700

    crypto/x509: update tests to pass in FIPS mode
    Update tests to use 2048 bit keys and certs in FIPS mode, and skip any
    non-approved ciphers.

--- src/crypto/x509/x509_test.go
+++ src/crypto/x509/x509_test.go
@@ -133,6 +133,36 @@ wg/HcAJWY60xZTJDFN+Qfx8ZQvBEin6c2/h+zZi5IVY=
 -----END RSA TESTING KEY-----
 `)
 
+var pemPrivateKey2048 = testingKey(`
+-----BEGIN RSA TESTING KEY-----
+MIIEpAIBAAKCAQEAn/H5NDeonggV+CDlSHyorbeJBnDkC3opgn7I6qh3MrDCFRhL
+RUjOhOAMteNX/+yKnVtFLSKjW+D14UTiA+8g8UdvOMS+l2wrr/7eYT91HHoWHN4Z
+i4slXTJGUImVrX1FCX/9ajrTPqITYOfS0VqeHfuol9JWtSS9xMAiHJJDYyfYUGxe
+z29gkvhEvWKOeSgGqfCjm5aXEfDAUjd/G6p8I30HQl7zhDo2pth1MSsxBV+TAPnw
+NJnuAoqAnAkS1ipfhqDfRfHwAX6Gfk/tnmpeAy7jvea10fEqziFQYd1xTq/0VYY7
+V3DVRWSnLw7enevFeowCgGCpuZ5Y/Rj6XeL/qwIDAQABAoIBAQCNpMZifd/vg42h
+HdCvLuZaYS0R7SunFlpoXEsltGdLFsnp0IfoJZ/ugFQBSAIIfLwMumU6oXA1z7Uv
+98aIYV61DePrTCDVDFBsHbNmP8JAo8WtbusEbwd5zyoB7LYG2+clkJklWE73KqUq
+rmI+UJeyScl2Gin7ZTxBXz1WPBk9VwcnwkeaXpgASIBW23fhECM9gnYEEwaBez5T
+6Me8d1tHtYQv7vsKe7ro9w9/HKrRXejqYKK1LxkhfFriyV+m8LZJZn2nXOa6G3gF
+Nb8Qk1Uk5PUBENBmyMFJhT4M/uuSq4YtMrrO2gi8Q+fPhuGzc5SshYKRBp0W4P5r
+mtVCtEFRAoGBAMENBIFLrV2+HsGj0xYFasKov/QPe6HSTR1Hh2IZONp+oK4oszWE
+jBT4VcnITmpl6tC1Wy4GcrxjNgKIFZAj+1x1LUULdorXkuG8yr0tAhG9zNyfWsSy
+PrSovC0UVbzr8Jxxla+kQVxEQQqWQxPlEVuL8kXaIDA6Lyt1Hpua2LvPAoGBANQZ
+c6Lq2T7+BxLxNdi2m8kZzej5kgzBp/XdVsbFWRlebIX2KrFHsrHzT9PUk3DE1vZK
+M6pzTt94nQhWSkDgCaw1SohElJ3HFIFwcusF1SJAc3pQepd8ug6IYdlpDMLtBj/P
+/5P6BVUtgo05E4+I/T3iYatmglQxTtlZ0RkSV2llAoGBALOXkKFX7ahPvf0WksDh
+uTfuFOTPoowgQG0EpgW0wRdCxeg/JLic3lSD0gsttQV2WsRecryWcxaelRg10RmO
+38BbogmhaF4xvgsSvujOfiZTE8oK1T43M+6NKsIlML3YILbpU/9aJxPWy0s2DqDr
+cQJhZrlk+pzjBA7Bnf/URdwxAoGAKR/CNw14D+mrL3YLbbiCXiydqxVwxv5pdZdz
+8thi3TNcsWC4iGURdcVqbfUinVPdJiXe/Kac3WGCeRJaFVgbKAOxLti1RB5MkIhg
+D8eyupBqk4W1L1gkrxqsdj4TFlxkwMywjl2E2S4YyQ8PBt6V04DoVRZsIKzqz+PF
+UionPq0CgYBCYXvqioJhPewkOq/Y5wrDBeZW1FQK5QD9W5M8/5zxd4rdvJtjhbJp
+oOrtvMdrl6upy9Hz4BJD3FXwVFiPFE7jqeNqi0F21viLxBPMMD3UODF6LL5EyLiR
+9V4xVMS8KXxvg7rxsuqzMPscViaWUL6WNVBhsD2+92dHxSXzz5EJKQ==
+-----END RSA TESTING KEY-----
+`)
+
 // pemEd25519Key is the example from RFC 8410, Secrion 4.
 var pemEd25519Key = `
 -----BEGIN PUBLIC KEY-----
@@ -176,6 +206,9 @@ func init() {
 	}
 	testPrivateKey2048.Precompute()
 
+	if boring.Enabled() {
+		testPrivateKey = testPrivateKey2048
+	}
 }
 
 func bigFromString(s string) *big.Int {
@@ -583,6 +616,27 @@ func parseURI(s string) *url.URL {
 	return uri
 }
 
+func isFipsAllowedSignatureAlgorithm(sigAlg SignatureAlgorithm) bool {
+	allowedSigAlgs := []SignatureAlgorithm{
+		SHA256WithRSAPSS,
+		SHA384WithRSAPSS,
+		SHA512WithRSAPSS,
+		SHA256WithRSA,
+		ECDSAWithSHA256,
+		SHA384WithRSA,
+		ECDSAWithSHA384,
+		SHA512WithRSA,
+		ECDSAWithSHA512,
+	}
+
+	for _, s := range allowedSigAlgs {
+		if sigAlg == s {
+			return true
+		}
+	}
+	return false
+}
+
 func TestCreateSelfSignedCertificate(t *testing.T) {
 	random := rand.Reader
 
@@ -686,6 +740,10 @@ func TestCreateSelfSignedCertificate(t *testing.T) {
 					Value:    []byte{0x04, 0x04, 4, 3, 2, 1},
 				},
 			},
+		}
+		if boring.Enabled() && !isFipsAllowedSignatureAlgorithm(test.sigAlgo) {
+			t.Logf("%s: boring enabled, signature algorithm %s is not supported", test.name, test.sigAlgo)
+			continue
 		}
 
 		derBytes, err := CreateCertificate(random, &template, &template, test.pub, test.priv)
@@ -1165,6 +1223,26 @@ func TestEd25519SelfSigned(t *testing.T) {
 	}
 }
 
+const pemCertificate2048 = `-----BEGIN CERTIFICATE-----
+MIIDFjCCAf6gAwIBAgIJAOjwnT/iW+qmMA0GCSqGSIb3DQEBCwUAMB8xCzAJBgNV
+BAoTAkdvMRAwDgYDVQQDEwdHbyBSb290MB4XDTE2MDEwMTAwMDAwMFoXDTI1MDEw
+MTAwMDAwMFowGjELMAkGA1UEChMCR28xCzAJBgNVBAMTAkdvMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn/H5NDeonggV+CDlSHyorbeJBnDkC3opgn7I
+6qh3MrDCFRhLRUjOhOAMteNX/+yKnVtFLSKjW+D14UTiA+8g8UdvOMS+l2wrr/7e
+YT91HHoWHN4Zi4slXTJGUImVrX1FCX/9ajrTPqITYOfS0VqeHfuol9JWtSS9xMAi
+HJJDYyfYUGxez29gkvhEvWKOeSgGqfCjm5aXEfDAUjd/G6p8I30HQl7zhDo2pth1
+MSsxBV+TAPnwNJnuAoqAnAkS1ipfhqDfRfHwAX6Gfk/tnmpeAy7jvea10fEqziFQ
+Yd1xTq/0VYY7V3DVRWSnLw7enevFeowCgGCpuZ5Y/Rj6XeL/qwIDAQABo1owWDAO
+BgNVHQ8BAf8EBAMCBaAwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMAwG
+A1UdEwEB/wQCMAAwGQYDVR0RBBIwEIIOZXhhbXBsZS5nb2xhbmcwDQYJKoZIhvcN
+AQELBQADggEBAHhkPAkkwEATgzwN59YvmC3sPmiP+PYD7HGBlLlzFzHTPc3QIbyR
+Iy1wMVHn/IC+hpWokcyVqFi6Pg+nLvb6O/a08ZzQKMRjLyXji4lgBMJTHvGGqAHq
+QEl9ZTk7bZTaFOLrKS7hzofn9b9UlceReQahKYzD3tKkHnR398D0xefN9dBccilB
+WVN+Jo9LdDgKf/HS6ufW7tXeZZaqlLp46ro5sT+HI2PmRZqgwP0pKdwSrNnuq2Li
+JAuLt61TT7oo2XY7nQP41qdR0bEV0MY0wjRSxbGy3pxI/6ENKHMUZCFWNbf/JPaI
+rRVICxf1XW1NuPC0iK8uOn/t96JySEdAkfU=
+-----END CERTIFICATE-----`
+
 const pemCertificate = `-----BEGIN CERTIFICATE-----
 MIIDATCCAemgAwIBAgIRAKQkkrFx1T/dgB/Go/xBM5swDQYJKoZIhvcNAQELBQAw
 EjEQMA4GA1UEChMHQWNtZSBDbzAeFw0xNjA4MTcyMDM2MDdaFw0xNzA4MTcyMDM2
@@ -1238,9 +1316,9 @@ MC4CAQAwBQYDK2VwBCIEINdKh2096vUBYu4EIFpjShsUSh3vimKya1sQ1YTT4RZG
 -----END TEST KEY-----`)
 
 func TestCRLCreation(t *testing.T) {
-	block, _ := pem.Decode([]byte(pemPrivateKey))
+	block, _ := pem.Decode([]byte(pemPrivateKey2048))
 	privRSA, _ := ParsePKCS1PrivateKey(block.Bytes)
-	block, _ = pem.Decode([]byte(pemCertificate))
+	block, _ = pem.Decode([]byte(pemCertificate2048))
 	certRSA, _ := ParseCertificate(block.Bytes)
 
 	block, _ = pem.Decode([]byte(ed25519CRLKey))
